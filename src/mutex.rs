2f7cdfd71b9999863691d0af06df74ee8cf79efb
joyel24 <joel.martin.2104@gmail.com>
2025-01-25 02:43:22 +0100

    Update mutex.rs tests

--- src/mutex.rs
+++ src/mutex.rs
@@ -144,7 +144,7 @@ impl<T> BusMutex for XtensaMutex<T> {
 
     fn lock<R, F: FnOnce(&mut Self::Bus) -> R>(&self, f: F) -> R {
         //xtensa_lx::interrupt::free(|_| f(&mut (*self.0.lock())))
-        xtensa_lx::interrupt::free(|_| f(&mut (self.lock()) )
+        xtensa_lx::interrupt::free(|_| f(&mut (self.lock())))
     }
 }
 
